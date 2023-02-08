faaf0d12d86c2f3c2c95e3d3ec0e5200334fbaef
vanireddy2003 <prvanireddy2003@gmail.com>
2023-02-08 12:20:21 +0530

    first commit

---
+++ app/templatetags/usdfilters.py
@@ -0,0 +1,19 @@
+from django import template
+
+register=template.Library()
+
+@register.filter(name='swap')
+def swapping(value):
+    return value.swapcase()
+
+@register.filter()
+def counting(value,arg):
+    c=0
+    for i in range(len(value)):
+        if value[i:i+len(arg)]==arg:
+            c=c+1
+    return c
+
+@register.filter()
+def remove(value,arg):
+    return value.replace(arg,'')
